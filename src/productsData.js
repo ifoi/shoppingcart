c2a4e5cbce259eb2dfe1533c90eec8ec679425ad
Ifo Ikede <ifoitech@gmail.com>
2022-07-29 01:27:43 -0300

    create product data

---
+++ src/productsData.js
@@ -0,0 +1,35 @@
+const productsData = {
+    products: [
+        {
+            id: '1',
+            name: "Djembe",
+            price: 400,
+            image: "https://picsum.photos/200/300" ,
+
+        },
+        {
+            id: '2',
+            name: "Small Djembe",
+            price: 400,
+            image: "https://picsum.photos/200/300" ,
+
+        },
+        {
+            id: '3',
+            name: " Medium Djembe",
+            price: 400,
+            image: "https://picsum.photos/200/300" ,
+
+        },
+        {
+            id: '4',
+            name: "Medium Dundun",
+            price: 400,
+            image: "https://picsum.photos/200/300" ,
+
+        },
+    ],
+
+};
+
+export default productsData ;
