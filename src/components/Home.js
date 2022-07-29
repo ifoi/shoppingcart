439579bf2c4e347df7cb18b0ba958c85dd86d799
Ifo Ikede <ifoitech@gmail.com>
2022-07-29 03:48:25 -0300

    add image of drums

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home
+        <img src="https://thumbs.dreamstime.com/b/drums-12878771.jpg" alt="Bunch of traditional handmade drums"></img>
+    </div>
+
+  )
+}
+
+export default Home
