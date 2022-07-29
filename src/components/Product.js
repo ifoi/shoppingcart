8fb0e7cd35385198e278ac8e6214a96c9d4101cc
Ifo Ikede <ifoitech@gmail.com>
2022-07-29 01:28:37 -0300

    initial product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,5 +2,19 @@ import React from 'react';
 
 export default function Product(props) {
 
-    return <div> Product Page</div> ;
+    const {product, addToCart } = props ;
+
+
+    return  (
+         <div> 
+         
+          <h2>{product.name } </h2>
+          <img src={product.image} alt={product.name} ></img>
+         <h3>{product.name}</h3>
+         <div>${product.price}</div>
+         <button onClick={addToCart}> Add to Cart</button>
+         </div> 
+
+    
+    );
 }
