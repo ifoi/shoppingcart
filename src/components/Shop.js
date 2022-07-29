e27124c71d30554f4259aee9687a1b3972afebde
Ifo Ikede <ifoitech@gmail.com>
2022-07-29 01:27:12 -0300

    basic list products functionality

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Product from './Product';
 
 export default function Shop(props) {
+    const {products} = props;
 
-    return <div> Shopping Page</div> ;
+    return (
+    <div> <h2>Products Page </h2>
+
+         {products.map((product) => (
+            <Product key={product.id} product={product}></Product>
+
+         )) }
+
+
+    </div>
+    );
 }
