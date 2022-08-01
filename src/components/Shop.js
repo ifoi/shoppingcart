c190fe9f21a90e319ae66cf761a1730825179e9a
Ifo Ikede <ifoitech@gmail.com>
2022-08-01 10:10:19 -0300

    make responsive

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Product from './Product';
-import {Container, Button} from 'react-bootstrap';
+import {Container, Button, Row, Col} from 'react-bootstrap';
 
 export default function Shop(props) {
     const {products, addToCart} = props;
@@ -9,11 +9,13 @@ export default function Shop(props) {
     <>
     <Container >
     <div> <h2>Products Page </h2>
-        
+         <Row md={2} xs={1} lg={3}> 
          {products.map((product) => (
-            <Product key={product.id} product={product} addToCart={addToCart}></Product>
-
+          <Col>
+          <Product key={product.id} product={product} addToCart={addToCart}></Product>
+            </Col>  
          )) }
+        </Row> ; 
 
 
     </div> </Container>
