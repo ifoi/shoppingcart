1a9ae918a9ecc3a73d7c26a3a7d692ffc2c21089
Ifo Ikede <ifoitech@gmail.com>
2022-08-26 23:23:41 -0300

    Update bootstrap classnames to page elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home
-        <img src="https://thumbs.dreamstime.com/b/drums-12878771.jpg" alt="Bunch of traditional handmade drums"></img>
+    <div className="align-items-center justify-content-center">
+        <img src="https://thumbs.dreamstime.com/b/drums-12878771.jpg" 
+          alt="Bunch of traditional handmade drums" >
+          </img>
     </div>
 
   )
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,17 +3,42 @@ import {Button, Card} from 'react-bootstrap';
 
 export default function Product(props) {
 
-    const {product, addToCart } = props ;
+    const {product, addToCart, removeFromCart} = props;
 
-
+   const quantity = 2 ;
     return  (
-         <Card > 
+       <Card ClassName="h-100"> 
          
           <h2>{product.name } </h2>
-          <Card.Img src={product.image} alt={product.name} ></Card.Img>
-         <h3>{product.name}</h3>
-         <div>${product.price}</div>
-         <Button onClick={addToCart}> Add to Cart</Button>
+          <Card.Img src={product.image}
+                 alt={product.name}
+                 height="200px"
+                //  style={{objectFit: "contain"}}
+            />
+
+          <Card.Body className="d-flex flex-column"> 
+            <Card.Title  className="d-flex justify-content-between">       
+                <span> {product.name}</span>
+                <span className="ms-2">${product.price}</span>
+                </Card.Title>
+
+          {quantity ===0 ? (
+            <Button onClick={addToCart}> Add to Cart</Button> )
+              : <>
+               <div className="align-items-center justify-content-center" 
+              style={{gap: ".5rem"}}> <Button> - </Button>  
+
+             {quantity} in cart 
+
+            <Button onClick={addToCart}> +</Button>  
+            <Button onClick={removeFromCart} className="btn btn-danger"> Remove</Button>
+            </div>
+            </>
+            }
+ 
+
+         </Card.Body>
+         
          </Card> 
 
     
